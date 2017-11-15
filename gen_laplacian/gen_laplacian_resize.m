d5ba1e21aa6d2a864fd49aa0f9d0a00e88d0d837
wangyannhao <wangyannhao@gmail.com>
2017-11-15 00:42:27 -0500

    add gen.. _resize

--- gen_laplacian/gen_laplacian_resize.m
+++ gen_laplacian/gen_laplacian_resize.m
@@ -4,7 +4,7 @@ N = 60;
 
 for i = 1:N
     i
-    prefix = '../examples/input/';
+    prefix = '../examples/input_resize/';
     in_name = [prefix 'in' int2str(i) '.png']; 
     input = im2double(imread(in_name));
     input = reshape_img(input, 700);
@@ -16,14 +16,14 @@ for i = 1:N
     n = nnz(A);
     [Ai, Aj, Aval] = find(A);
     CSC = [Ai, Aj, Aval];
-    %save(['/home/wangyannhao/Github/671/deep-photo-styletransfer/gen_laplacian/matrix/Input_Laplacian_3x3_1e-7_CSC' int2str(i) '.mat'], 'CSC');
+    %save(['/home/wangyannhao/Github/671/deep-photo-styletransfer/gen_laplacian/matrix_resize/Input_Laplacian_3x3_1e-7_CSC' int2str(i) '.mat'], 'CSC');
     
     [rp ci ai] = sparse_to_csr(A);
     Ai = sort(Ai);
     Aj = ci;
     Aval = ai;
     CSR = [Ai, Aj, Aval];
-    save(['/home/wangyannhao/Github/671/deep-photo-styletransfer/gen_laplacian/matrix/Input_Laplacian_3x3_1e-7_CSR' int2str(i) '.mat'], 'CSR');
+    save(['/home/wangyannhao/Github/671/deep-photo-styletransfer/gen_laplacian/matrix_resize/Input_Laplacian_3x3_1e-7_CSR' int2str(i) '.mat'], 'CSR');
  
 end 
 
